292990e088d80e6775268c01cf7c4f25cd018e68
Arial Brewer <arial.brewer@gmail.com>
2024-08-09 08:58:29 -0700

    updated code

--- CIB_Tide_analysis.R
+++ CIB_Tide_analysis.R
@@ -1,4 +1,5 @@
 #HOBO vs NOAA tide plots
+#call rate model with HOBO water level as covariate
 
 #load libraries
 library(tidyverse)
@@ -18,8 +19,8 @@ tide_total <- read.csv("2022_combined tide data.csv") %>%
 
 #plots of all days
 ggplot(data=tide_total) + 
-  geom_line(aes(x=time,y=water_level_HOBO,group=date,color="red")) +
-  geom_line(aes(x=time,y=water_level_NOAA,group=date,color="blue")) +
+  geom_line(aes(x=time,y=water_level_HOBO,group=date),color="blue") +
+  geom_line(aes(x=time,y=water_level_NOAA,group=date),color="red")+
   theme_classic() +
   labs(x="Time", y="Water level") +
   scale_x_time() +
@@ -28,8 +29,8 @@ ggplot(data=tide_total) +
   
 #individual day
 ggplot(data=tide_total %>% filter(date=="2022-08-08")) + 
-  geom_line(aes(x=time,y=water_level_HOBO,group=date,color="red")) +
-  geom_line(aes(x=time,y=water_level_NOAA,group=date,color="blue")) +
+  geom_line(aes(x=time,y=water_level_HOBO,group=date),color="blue") +
+  geom_line(aes(x=time,y=water_level_NOAA,group=date),color="red") +
   theme_classic() +
   labs(x="Time", y="Water level") +
   scale_x_time() 
@@ -68,18 +69,17 @@ callrate_total <- data_total %>%
          tide = as.factor(tide),
          encounter = as.factor(encounter))
 
-#bin by 8 minutes
-breaks <- seq(0,8,1)
-
-bin_data <- callrate_total %>% 
-  mutate(bin_durations= cut(time,breaks=breaks)) %>% 
-  group_by(encounter,time,bin_durations) %>% 
-  summarise(bin_n_miunte=sum(n_minute))
-
-bin_data2<- callrate_total %>% 
-  group_by(encounter,time=floor_hms(time,"8minute")) %>% 
-  summarise(bin_n_minute=sum(n_minute))
-
+#bin by 8 minutes- not working
+# breaks <- seq(0,8,1)
+# 
+# bin_data <- callrate_total %>% 
+#   mutate(bin_durations= cut(time,breaks=breaks)) %>% 
+#   group_by(encounter,time,bin_durations) %>% 
+#   summarise(bin_n_miunte=sum(n_minute))
+# 
+# bin_data2<- callrate_total %>% 
+#   group_by(encounter,time=floor_hms(time,"8minute")) %>% 
+#   summarise(bin_n_minute=sum(n_minute))
 
 
 
@@ -93,61 +93,92 @@ new_data <- callrate_total %>%
   left_join(hobo, by = c("date","time"), multiple = "all")
 
 
-###run call rate model
-#poisson
-#with just tide and encounter
-test0<-glmer(n_minute ~ water_level_HOBO + (1|encounter),
+####### Run call rate model
+#Poisson with just tide and encounter
+pois.uni<-glmer(n_minute ~ water_level_HOBO + (1|encounter),
              family=poisson(link="log"), data=new_data)
 
-#with all covariates
-test0<-glmer(n_minute ~ behavior + group_size + calf_presence + tide + water_level_HOBO + (1|encounter),
-            family=poisson(link="log"), data=new_data)
-
-summary(test0)
+summary(pois.uni)
+plot(parameters(pois.uni))
 
 #check overdispersion parameter manually (X2/df.resid) Overdispersed > 1
-sum(residuals(test0,type="pearson")^2)/79
+sum(residuals(pois.uni,type="pearson")^2)/83  #overdispersed
 
 #check overdispersion with performance package
-check_overdispersion(test0) #overdispersed
+check_overdispersion(pois.uni) #overdispersed
 
 #check zero-inflation
-check_zeroinflation(test0)  #zero-inflation
+check_zeroinflation(pois.uni)  #zero-inflation
 
-#check residuals- better with nb
-simulateResiduals(fittedModel = test0, plot = T)
+
+
+#Poisson with all covariates
+pois.multi<-glmer(n_minute ~ behavior + group_size + calf_presence + tide + water_level_HOBO + (1|encounter),
+            family=poisson(link="log"), data=new_data)
+
+summary(pois.multi)
+plot(parameters(pois.multi))
+
+#check overdispersion parameter manually (X2/df.resid) Overdispersed > 1
+sum(residuals(pois.multi,type="pearson")^2)/79   #overdispersed
+
+#check overdispersion with performance package
+check_overdispersion(pois.multi) #overdispersed
+
+#check zero-inflation
+check_zeroinflation(pois.multi)  #zero-inflation
+
+#check residuals
+simulateResiduals(fittedModel = pois.multi, plot = T)
 
 
 ### Poisson model is overdispersed and zero-inflated, use negative binomial
-##model with tide data only
-test1<-glmer.nb(n_minute ~ water_level_HOBO + (1|encounter),
+##NB model with just tide and encounter
+nb.uni<-glmer.nb(n_minute ~ water_level_HOBO + (1|encounter),
                   data=new_data)
 
-summary(test1)
-plot(parameters(test1))
-
-#Fit negative binomial model with all covariates
-#log(group_size)=0.35 so can't use offset
-test2<-glmer.nb(n_minute ~ behavior + log(group_size) + calf_presence + tide + water_level_HOBO + (1|encounter),
-                data=new_data)
-
-test2<-glmer.nb(n_minute ~ behavior + group_size + calf_presence + tide + water_level_HOBO + (1|encounter),
-                data=new_data)
-
-summary(test2)
-plot(parameters(test2))
+summary(nb.uni)
+plot(parameters(nb.uni))
 
 #check overdispersion parameter manually (X2/df.resid)
-sum(residuals(test2,type="pearson")^2)/78
+sum(residuals(nb.uni,type="pearson")^2)/82  #no over-dispersion
 
 #check overdispersion with performance package
-check_overdispersion(test2)  #no over-dispersion
+check_overdispersion(nb.uni)  #no over-dispersion
 
 #check zero-inflation
-check_zeroinflation(test2)   #no zero-inflation
+check_zeroinflation(nb.uni)   #no zero-inflation
 
 #check residuals
-simulateResiduals(fittedModel = test2, plot = T)
+simulateResiduals(fittedModel = nb.uni, plot = T)
+
+
+#NB model with all covariates
+#log(group_size)=0.35 so can't use offset
+test<-glmer.nb(n_minute ~ behavior + log(group_size) + calf_presence + tide + water_level_HOBO + (1|encounter),
+                data=new_data)
+
+summary(test)
+plot(parameters(test))
+
+
+nb.multi<-glmer.nb(n_minute ~ behavior + group_size + calf_presence + tide + water_level_HOBO + (1|encounter),
+                data=new_data)
+
+summary(nb.multi)
+plot(parameters(nb.multi))
+
+#check overdispersion parameter manually (X2/df.resid)
+sum(residuals(nb.multi,type="pearson")^2)/78
+
+#check overdispersion with performance package
+check_overdispersion(nb.multi)  #no over-dispersion
+
+#check zero-inflation
+check_zeroinflation(nb.multi)   #no zero-inflation
+
+#check residuals
+simulateResiduals(fittedModel = nb.multi, plot = T)
 
 
 
